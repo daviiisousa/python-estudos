82d6af845f3d9c4bbfb5666f2f7588acad3cfe94
daviiisousa <sousadavi248@gmail.com>
2024-09-24 16:38:22 -0300

    mudei a forma de fazer a media para se usada com um for

--- Atlantico/Aula2/media.py
+++ Atlantico/Aula2/media.py
@@ -1,6 +1,15 @@
-Nota1 = float(input("digite sua nota "))
+"""Nota1 = float(input("digite sua nota "))
 Nota2 = float(input("Digite sua nota "))
 Nota3 = float(input("digite sua nota "))
 
 media = (Nota1 + Nota2 + Nota3) / 3
-print("Sua media é ", media)
+print("Sua media é ", media)"""
+
+media = 0
+
+for i in range(1, 4):
+    nota = float(input(f"digite sua nota {i} "))
+
+    media += nota
+
+print(media // 3)
