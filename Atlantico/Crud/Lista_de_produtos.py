caea4e16b1f89326560874fb132611151a20dd76
daviiisousa <sousadavi248@gmail.com>
2024-09-28 17:32:54 -0300

    atuyalizaçoes

--- Atlantico/Crud/Lista_de_produtos.py
+++ Atlantico/Crud/Lista_de_produtos.py
@@ -1,13 +1,23 @@
 def exibir_lista_de_frutas_e_qtd( qtd, lista ):
-    for i in qtd:
-        print(f"{i} quantidade em estoque ")
+    for i in range(len(lista)):
+        print(f"{qtd[i]} {lista[i]} em estoque  ")
 
-    for i in lista:
-        print(f"{i} ")
-    
 def adicionar_nova_fruta(lista, qtd):
     nova_fruta = input("adicione uma nova fruta: ")
     lista.append(nova_fruta)
+
+    nova_qtd = input("qual a quantidade? ")
+    qtd.append(nova_qtd)
+
+def adicionar_mais_uma_fruta(lista, qtd):
+    nova_fruta = input("deseja adicionar mais uma fruta (sim/nao) ? ")
+    if nova_fruta == "sim":
+        nova_fruta = input("qual a fruta ? ")
+        lista.append(nova_fruta)
+    elif nova_fruta == "nao":
+        return
+    else:
+        print("pfv tente novamente e digite uma opçao valida")
 
     nova_qtd = input("qual a quantidade? ")
     qtd.append(nova_qtd)
@@ -18,7 +28,12 @@ lista_qtd = [10, 20, 30]
 print("Lista de Frutas e quantidade: ")
 exibir_lista_de_frutas_e_qtd(lista_qtd, lista_de_frutas)
 
-
-print()
 adicionar_nova_fruta(lista_de_frutas, lista_qtd)
-print("nova lista de fruta e quantidade: ", lista_de_frutas, lista_qtd)
+
+adicionar_mais_uma_fruta(lista_de_frutas, lista_qtd)
+print("Nova Lista: ")
+print(f"{lista_qtd[0]} {lista_de_frutas[0]} em estoque")
+print(f"{lista_qtd[1]} {lista_de_frutas[1]} em estoque")
+print(f"{lista_qtd[2]} {lista_de_frutas[2]} em estoque")
+print(f"{lista_qtd[3]} {lista_de_frutas[3]} em estoque")
+print(f"{lista_qtd[4]} {lista_de_frutas[4]} em estoque")
